ec4bada272881c0c32f7e4dc34d40d2f97c33c1f
Tona <antoniojesusjimenezpolonio@gmail.com>
2025-11-10 19:56:02 +0100

    modifico el action.jsx para agregar las funciones de cliente

--- src/lib/action.js
+++ src/lib/action.js
@@ -5,7 +5,7 @@ import { revalidatePath } from 'next/cache'
 
 
 
-// BASE DE DATOS
+// BASE DE DATOS - productos
 
 
 export async function nuevoProductoDB(formData) {
@@ -51,8 +51,7 @@ export async function eliminarProductoDB(formData) {
 
 
 
-// API
-
+// API - Productos
 export async function nuevoProductoAPI(formData) {
     const [nombre, descripcion, precio] = formData.values()
 
@@ -84,4 +83,68 @@ export async function eliminarProductoAPI(formData) {
     await fetch('http://localhost:3001/productos/' + id, { method: 'DELETE' })
 
     revalidatePath('/productos-api')
+}
+
+
+
+// BASE DE DATOS - clientes
+
+export async function nuevoClienteDB(formData) {
+  const nombre = formData.get('nombre');
+  const edad = formData.get('edad');
+
+  const sql = 'INSERT INTO `clientes` (`nombre`, `edad`) VALUES (?, ?)';
+  const values = [nombre, +edad];
+  await db.query(sql, values);
+  revalidatePath('/clientes-db');
+}
+
+export async function editarClienteDB(formData) {
+  const id = formData.get('id');
+  const nombre = formData.get('nombre');
+  const edad = formData.get('edad');
+
+  const sql = 'UPDATE clientes SET nombre=?, edad=? WHERE id=?';
+  const values = [nombre, +edad, id];
+  await db.query(sql, values);
+  revalidatePath('/clientes-db');
+}
+
+export async function eliminarClienteDB(formData) {
+  const id = formData.get('id');
+  await db.query('DELETE FROM clientes WHERE id = ?', [id]);
+  revalidatePath('/clientes-db');
+}
+
+
+//  API - Clientes
+
+export async function nuevoClienteAPI(formData) {
+  const [nombre, edad] = formData.values();
+
+  const response = await fetch('http://localhost:3001/clientes', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ nombre, edad: +edad })
+  });
+  await response.json();
+  revalidatePath('/clientes-api');
+}
+
+export async function editarClienteAPI(formData) {
+  const [id, nombre, edad] = formData.values();
+
+  const response = await fetch('http://localhost:3001/clientes/' + id, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ nombre, edad: +edad })
+  });
+  await response.json();
+  revalidatePath('/clientes-api');
+}
+
+export async function eliminarClienteAPI(formData) {
+  const id = formData.get('id');
+  await fetch('http://localhost:3001/clientes/' + id, { method: 'DELETE' });
+  revalidatePath('/clientes-api');
 }
